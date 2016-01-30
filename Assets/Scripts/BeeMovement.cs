25158f473078224ac86c0779228340a2a9cf9dd0
sephirot47 <victorantondominguez@gmail.com>
2016-01-30 02:24:29 +0100

    Bee move done

--- Assets/Scripts/BeeMovement.cs
+++ Assets/Scripts/BeeMovement.cs
@@ -1,15 +1,34 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class BeeMovement : MonoBehaviour {
+public class BeeMovement : MonoBehaviour
+{
+    public float rotSpeed;
+    public float speed;
+    private Rigidbody rb;
 
-	// Use this for initialization
-	void Start () {
-	
+	void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+	void Update ()
+    {
+        float inputx = Input.GetAxis("Horizontal");
+        float inputz = Input.GetAxis("Vertical");
+        
+        transform.position += new Vector3(inputx, 0.0f, inputz) * speed * Time.deltaTime;
+
+        /*Quaternion rot = Quaternion.Lerp(transform.rotation, 
+                                         Quaternion.LookRotation(new Vector3(inputx, 0.0f, inputz), Vector3.up), 
+                                         Time.deltaTime * rotSpeed);
+        */
+
+        transform.LookAt(transform.position + new Vector3(inputx, 0.0f, inputz));
+         
+        if (Input.GetButton("Recolect"))
+        {
+            Debug.Log("Recolect");
+        }
+    }
 }
