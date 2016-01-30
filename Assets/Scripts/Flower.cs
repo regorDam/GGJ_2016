f51ba89028e7022b7edf767cbc0f5c2edfb746dc
sephirot47 <victorantondominguez@gmail.com>
2016-01-30 03:38:23 +0100

    Mec

--- Assets/Scripts/Flower.cs
+++ Assets/Scripts/Flower.cs
@@ -3,16 +3,17 @@ using System.Collections;
 
 public class Flower : MonoBehaviour
 {
+    public int userid;
     public int polen;
     public float cooldown;
 
 	void Start ()
     {
-        GetComponent<Renderer>().material.color = Color.blue;
+        userid = Random.Range(0, 4);
 	}
 	
 	void Update ()
     {
-	    
-	}
+        GetComponent<Renderer>().material.color = Game.game.GetUserColor(userid);
+    }
 }
---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Game : MonoBehaviour
+{
+    public static Game game;
+    public List<Color> userColors;
+
+	void Start ()
+    {
+	    game = this;
+	}
+	
+	void Update ()
+    {
+	
+	}
+
+    public Color GetUserColor(int userid)
+    {
+        return userColors[userid];
+    }
+}
