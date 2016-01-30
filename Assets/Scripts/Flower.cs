e38f295bcd1e946a60115ccd90ea60713451df67
sephirot47 <victorantondominguez@gmail.com>
2016-01-30 01:25:51 +0100

    FlowerGenerator begun 2

---
+++ Assets/Scripts/Flower.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Flower : MonoBehaviour
+{
+    public int polen;
+    public float cooldown;
+
+	void Start ()
+    {
+        GetComponent<Renderer>().material.color = Color.blue;
+	}
+	
+	void Update ()
+    {
+	    
+	}
+}
